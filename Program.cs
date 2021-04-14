89366564ce377195c7f75dcfe8da785b8fa2a0f0
ekta12345 <ektakumbhare3@gmail.com>
2021-04-14 12:12:55 +0530

    Starting the snake and ladder game

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeNLadder
+{
+    class Program
+    {
+        const int startPoint =0;
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Starting position for player1  " + startPoint);
+        }
+        
+    }
+}
